be6263fd1d00187abe41e57b032fce1932d47cfe
DavidChavez1 <davidchavez0103@gmail.com>
2019-10-23 17:09:31 -0500

    Actualizar app

--- crudMySQL9/app/src/main/java/com/example/crudmysql9/MainActivity.java
+++ crudMySQL9/app/src/main/java/com/example/crudmysql9/MainActivity.java
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +22,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         email = findViewById(R.id.idEmail);
         btn_regUser = findViewById(R.id.idbtnRegister);
+    }
+
+    private void Register(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(MainActivity.this, ".--Saved--.", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            
+
+        };
     }
 }
